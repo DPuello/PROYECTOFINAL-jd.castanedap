1669af9a90465b5b95366f89eb7b894eca41d5d6
_JuanDaniel <juancast9808@gmail.com>
2024-12-01 12:32:19 -0500

    mysql to sqlite

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
-from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
 from flask_login import LoginManager
 from database.db import db, init_db
 from controllers.icecreamshop_controller import icecreamshop_blueprint
@@ -10,9 +10,12 @@ from controllers.product_controller import product_blueprint
 from controllers.user_controller import user_blueprint
 from models.user import User
 
-load_dotenv()
+file_path = os.path.join(os.path.abspath(os.getcwd()), "database", "site.db")
+
+# load_dotenv()
 app = Flask(__name__, template_folder="views")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_LINK")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + file_path
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_LINK")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev")
 
